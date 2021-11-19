7c6b303877a8a469191e649d0888c275af921f24
seeers <seeers@gmx.de>
2021-11-19 21:21:03 +0100

    adaptation to my hardware

--- bootloader-avr-mcp2515/config.h
+++ bootloader-avr-mcp2515/config.h
@@ -11,7 +11,7 @@
 
 #define BOOTLOADER_TYPE     0
 
-#define BOOT_LED            B,1
+//#define BOOT_LED            B,1
 
 //#define   BOOT_INIT
 //#define   BOOT_LED_SET_OUTPUT
@@ -21,5 +21,5 @@
 
 // CAN Settings
 
-#define MCP2515_CS          B,4
-#define MCP2515_INT         B,2
+#define MCP2515_CS          B,2
+#define MCP2515_INT         B,1
